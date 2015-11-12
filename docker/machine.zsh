1b3ece8dfcb105e4961f46094d0b0c35bef772ff
Joshua Delsman <j@srv.im>
2015-11-11 18:12:52 -0800

    updates 11/11

--- docker/machine.zsh
+++ docker/machine.zsh
@@ -6,10 +6,15 @@ if [ "$(docker-machine ls -q dev | wc -l)" -eq 0 ]; then
       rm -rf ~/.docker/machine/cache/boot2docker.iso
       docker-machine create -d virtualbox \
         --virtualbox-memory "4096" \
+        --virtualbox-cpu-count "-1" \
         --virtualbox-disk-size "60000" \
         dev
       ;;
   esac
+fi
+
+if [ "$(docker-machine status dev)" != "Running" ]; then
+  docker-machine start dev
 fi
 
 eval $(docker-machine env dev)
--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -1,4 +1,9 @@
-alias be="bundle exec"
+alias b="bundle"
+alias bi="b install --path vendor"
+alias bil="bi --local"
+alias bu="b update"
+alias be="b exec"
+alias binit="bi && b package && echo 'vendor/ruby' >> .gitignore"
 
 function deploy() {
   local deployto=$1
